95f7de3fb73f776e17859becc709d9e0fdd85654
olya <olyamelnyk2005@gmail.com>
2023-10-23 22:53:02 +0300

    Add project files.

---
+++ hw8/Program.cs
@@ -0,0 +1,48 @@
+﻿using hw8;
+
+List<Shape> shapes = new List<Shape>();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Enter name of {i + 1} circle: ");
+    string nameC = Console.ReadLine();
+    Console.WriteLine($"Enter radius of {i + 1} circle: ");
+    double radius = Convert.ToDouble( Console.ReadLine() );
+    shapes.Add(new Circle(nameC, radius));
+
+    Console.WriteLine($"Enter name of {i + 1} square: ");
+    string nameS = Console.ReadLine();
+    Console.WriteLine($"Enter side of {i + 1} square: ");
+    double side = Convert.ToDouble(Console.ReadLine());
+    shapes.Add(new Square(nameS, side));
+}
+
+foreach (Shape shape in shapes)
+{
+    if(shape.GetType() == typeof(Circle))
+        Console.WriteLine($"Circle name: {shape.Name}, area: {shape.Area()}, perimeter: {shape.Perimeter()}");
+    if (shape.GetType() == typeof(Square))
+        Console.WriteLine($"Square name: {shape.Name}, area: {shape.Area()}, perimeter: {shape.Perimeter()}");
+}
+
+double maxPerimeter = double.MinValue;
+string nameMaxPerimeter = string.Empty;
+
+foreach (Shape shape in shapes)
+{
+    double perimeter = shape.Perimeter();
+    if(perimeter > maxPerimeter)
+    {
+        maxPerimeter = perimeter;
+        nameMaxPerimeter = shape.Name;
+    }
+}
+
+Console.WriteLine($"Shape with the largest perimeter: {nameMaxPerimeter}");
+
+shapes.Sort();
+Console.WriteLine("After sorting shapes by area:");
+foreach (Shape shape in shapes)
+{
+    Console.WriteLine($"Name: {shape.Name}, area: {shape.Area()}, perimeter: {shape.Perimeter()}");
+}
